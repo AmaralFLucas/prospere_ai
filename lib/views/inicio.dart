51563802a3fbe979a9da86fe576fc96b47efbd18
HenriquePrudencio <141193621+HenriquePrudencio@users.noreply.github.com>
2024-11-23 11:53:23 -0300

    Update inicio.dart

--- lib/views/inicio.dart
+++ lib/views/inicio.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:prospere_ai/services/autenticacao.dart';
@@ -19,6 +20,7 @@ class Inicio extends StatefulWidget {
 class _InicioState extends State<Inicio> with SingleTickerProviderStateMixin {
   late PageController pageController;
   final AutenticacaoServico _autenServico = AutenticacaoServico();
+  String uid = FirebaseAuth.instance.currentUser!.uid;
 
   double totalReceitas = 0.0;
   double totalDespesas = 0.0;
@@ -29,9 +31,9 @@ class _InicioState extends State<Inicio> with SingleTickerProviderStateMixin {
 
   int initialPosition = 0;
   Color myColor = const Color.fromARGB(255, 30, 163, 132);
-Color myColor2 = const Color.fromARGB(255, 178, 0, 0);
-Color cardColor = const Color(0xFFF4F4F4);
-Color textColor = Colors.black87;
+  Color myColor2 = const Color.fromARGB(255, 178, 0, 0);
+  Color cardColor = const Color(0xFFF4F4F4);
+  Color textColor = Colors.black87;
 
   @override
   void initState() {
@@ -49,42 +51,43 @@ Color textColor = Colors.black87;
   Future<void> loadCategorias() async {
     categoriasReceitas = await getCategorias(widget.userId, 'receita');
     categoriasDespesas = await getCategorias(widget.userId, 'despesa');
-    setState(
-        () {}); // Atualizar o estado para garantir que os ícones sejam carregados
+    setState(() {});
   }
 
   @override
-Widget build(BuildContext context) {
-  return Scaffold(
-    drawer: Drawer(
-      child: ListView(
-        padding: EdgeInsets.zero,
-        children: <Widget>[
-          DrawerHeader(
-            decoration: const BoxDecoration(
-              color: Colors.blue,
+  Widget build(BuildContext context) {
+    return Scaffold(
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              decoration: const BoxDecoration(
+                color: Colors.blue,
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Icon(Icons.people, size: 100, color: Colors.white),
+                  IconButton(
+                    padding: const EdgeInsets.only(left: 135, bottom: 100),
+                    icon: const Icon(Icons.settings,
+                        size: 25, color: Colors.white),
+                    onPressed: () {
+                      Navigator.of(context).push(
+                        MaterialPageRoute(
+                            builder: (context) => const Configuracoes()),
+                      );
+                    },
+                  ),
+                ],
+              ),
             ),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Icon(Icons.people, size: 100, color: Colors.white),
-                IconButton(
-                  padding: const EdgeInsets.only(left: 135, bottom: 100),
-                  icon: const Icon(Icons.settings, size: 25, color: Colors.white),
-                  onPressed: () {
-                    Navigator.of(context).push(
-                      MaterialPageRoute(
-                          builder: (context) => const Configuracoes()),
-                    );
-                  },
-                ),
-              ],
-            ),
-          ),
-          ElevatedButton(
+            ElevatedButton(
               onPressed: () {
                 Navigator.of(context).push(
-                  MaterialPageRoute(builder: (context) => const MeuCadastro()),
+                  MaterialPageRoute(
+                      builder: (context) => MeuCadastro(userId: uid)),
                 );
               },
               style: ElevatedButton.styleFrom(
@@ -124,17 +127,19 @@ Widget build(BuildContext context) {
           ],
         ),
       ),
-    appBar: AppBar(
-      backgroundColor: myColor,
-      title: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Text(
-            widget.title ?? 'Inicio',
-            style: const TextStyle(
-                fontSize: 20, fontWeight: FontWeight.bold, color: Colors.white),
-          ),
-          Stack(
+      appBar: AppBar(
+        backgroundColor: myColor,
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              widget.title ?? 'Inicio',
+              style: const TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white),
+            ),
+            Stack(
               children: [
                 IconButton(
                   iconSize: 45,
@@ -168,151 +173,147 @@ Widget build(BuildContext context) {
           ],
         ),
       ),
-    body: StreamBuilder<QuerySnapshot>(
-      stream: FirebaseFirestore.instance
-          .collection('users')
-          .doc(widget.userId)
-          .collection('receitas')
-          .snapshots(),
-      builder: (context, receitasSnapshot) {
-        if (!receitasSnapshot.hasData)
-          return Center(child: CircularProgressIndicator());
-        return StreamBuilder<QuerySnapshot>(
-          stream: FirebaseFirestore.instance
-              .collection('users')
-              .doc(widget.userId)
-              .collection('despesas')
-              .snapshots(),
-          builder: (context, despesasSnapshot) {
-            if (!despesasSnapshot.hasData)
-              return Center(child: CircularProgressIndicator());
+      body: StreamBuilder<QuerySnapshot>(
+        stream: FirebaseFirestore.instance
+            .collection('users')
+            .doc(widget.userId)
+            .collection('receitas')
+            .snapshots(),
+        builder: (context, receitasSnapshot) {
+          if (!receitasSnapshot.hasData)
+            return Center(child: CircularProgressIndicator());
+          return StreamBuilder<QuerySnapshot>(
+            stream: FirebaseFirestore.instance
+                .collection('users')
+                .doc(widget.userId)
+                .collection('despesas')
+                .snapshots(),
+            builder: (context, despesasSnapshot) {
+              if (!despesasSnapshot.hasData)
+                return Center(child: CircularProgressIndicator());
 
-            List<Map<String, dynamic>> receitas = receitasSnapshot.data!.docs
-                .map((doc) => doc.data() as Map<String, dynamic>)
-                .toList();
-            List<Map<String, dynamic>> despesas = despesasSnapshot.data!.docs
-                .map((doc) => doc.data() as Map<String, dynamic>)
-                .toList();
+              List<Map<String, dynamic>> receitas = receitasSnapshot.data!.docs
+                  .map((doc) => doc.data() as Map<String, dynamic>)
+                  .toList();
+              List<Map<String, dynamic>> despesas = despesasSnapshot.data!.docs
+                  .map((doc) => doc.data() as Map<String, dynamic>)
+                  .toList();
 
-            totalReceitas = receitas.fold(
-                0.0, (sum, item) => sum + (item['valor'] ?? 0.0));
-            totalDespesas = despesas.fold(
-                0.0, (sum, item) => sum + (item['valor'] ?? 0.0));
-            saldoAtual = totalReceitas - totalDespesas;
+              totalReceitas = receitas.fold(
+                  0.0, (sum, item) => sum + (item['valor'] ?? 0.0));
+              totalDespesas = despesas.fold(
+                  0.0, (sum, item) => sum + (item['valor'] ?? 0.0));
+              saldoAtual = totalReceitas - totalDespesas;
 
-            // Filtrando apenas as despesas para exibição na lista
-            List<Map<String, dynamic>> transacoes = despesas.map((d) => {
-                  ...d,
-                  'tipo': 'despesa'
-                }).toList();
+              List<Map<String, dynamic>> transacoes =
+                  despesas.map((d) => {...d, 'tipo': 'despesa'}).toList();
 
-            transacoes.sort((a, b) =>
-                (b['data'] as Timestamp).compareTo(a['data'] as Timestamp));
+              transacoes.sort((a, b) =>
+                  (b['data'] as Timestamp).compareTo(a['data'] as Timestamp));
 
-            return SingleChildScrollView(
-              padding: const EdgeInsets.all(16),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  _buildBalanceCard(),
-                  const SizedBox(height: 20),
-                  _buildTransactionList(transacoes),
-                ],
-              ),
-            );
-          },
-        );
-      },
-    ),
-    floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
-  );
-}
+              return SingleChildScrollView(
+                padding: const EdgeInsets.all(16),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    _buildBalanceCard(),
+                    const SizedBox(height: 20),
+                    _buildTransactionList(transacoes),
+                  ],
+                ),
+              );
+            },
+          );
+        },
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
+    );
+  }
 
   Widget _buildBalanceCard() {
-  // Definir cor dinâmica para o saldo
-  Color saldoColor = saldoAtual >= 0 ? myColor: myColor2;
-  
-  return Container(
-    padding: const EdgeInsets.all(20),
-    decoration: BoxDecoration(
-      color: cardColor,
-      borderRadius: BorderRadius.circular(15),
-      boxShadow: const [
-        BoxShadow(
-          color: Colors.black12,
-          blurRadius: 8,
-          offset: Offset(0, 4),
-        ),
-      ],
-    ),
-    child: Column(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+    Color saldoColor = saldoAtual >= 0 ? myColor : myColor2;
+
+    return Container(
+      padding: const EdgeInsets.all(20),
+      decoration: BoxDecoration(
+        color: cardColor,
+        borderRadius: BorderRadius.circular(15),
+        boxShadow: const [
+          BoxShadow(
+            color: Colors.black12,
+            blurRadius: 8,
+            offset: Offset(0, 4),
+          ),
+        ],
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Text(
+            'Saldo Atual',
+            style: TextStyle(
+              fontSize: 22,
+              fontWeight: FontWeight.bold,
+              color: textColor,
+            ),
+          ),
+          const SizedBox(height: 10),
+          Text(
+            'R\$ ${saldoAtual.toStringAsFixed(2)}',
+            style: TextStyle(
+              fontSize: 28,
+              fontWeight: FontWeight.bold,
+              color: saldoColor,
+            ),
+          ),
+          const SizedBox(height: 20),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              _buildBalanceDetail(
+                'Receitas',
+                'R\$ ${totalReceitas.toStringAsFixed(2)}',
+                myColor,
+              ),
+              Container(
+                height: 40,
+                width: 1,
+                color: Colors.black26,
+              ),
+              _buildBalanceDetail(
+                'Despesas',
+                'R\$ ${totalDespesas.toStringAsFixed(2)}',
+                myColor2,
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildBalanceDetail(String title, String value, Color valueColor) {
+    return Column(
       children: [
         Text(
-          'Saldo Atual',
+          value,
           style: TextStyle(
-            fontSize: 22,
+            fontSize: 20,
             fontWeight: FontWeight.bold,
-            color: textColor,
+            color: valueColor,
           ),
         ),
         const SizedBox(height: 10),
         Text(
-          'R\$ ${saldoAtual.toStringAsFixed(2)}',
+          title,
           style: TextStyle(
-            fontSize: 28,
-            fontWeight: FontWeight.bold,
-            color: saldoColor, // Usar a cor dinâmica do saldo
+            fontSize: 16,
+            color: textColor.withOpacity(0.7),
           ),
         ),
-        const SizedBox(height: 20),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            _buildBalanceDetail(
-              'Receitas',
-              'R\$ ${totalReceitas.toStringAsFixed(2)}',
-              myColor, // Verde para receitas
-            ),
-            Container(
-              height: 40,
-              width: 1,
-              color: Colors.black26,
-            ),
-            _buildBalanceDetail(
-              'Despesas',
-              'R\$ ${totalDespesas.toStringAsFixed(2)}',
-              myColor2, // Vermelho para despesas
-            ),
-          ],
-        ),
       ],
-    ),
-  );
-}
-
-Widget _buildBalanceDetail(String title, String value, Color valueColor) {
-  return Column(
-    children: [
-      Text(
-        value,
-        style: TextStyle(
-          fontSize: 20,
-          fontWeight: FontWeight.bold,
-          color: valueColor, // Usar a cor dinâmica para valores
-        ),
-      ),
-      const SizedBox(height: 10),
-      Text(
-        title,
-        style: TextStyle(
-          fontSize: 16,
-          color: textColor.withOpacity(0.7),
-        ),
-      ),
-    ],
-  );
-}
+    );
+  }
 
   Widget _buildTransactionList(List<Map<String, dynamic>> transacoes) {
     return Container(
@@ -347,7 +348,6 @@ Widget _buildBalanceDetail(String title, String value, Color valueColor) {
       DateTime dateTime = timestamp.toDate();
       String formattedDate = DateFormat('dd/MM/yyyy').format(dateTime);
 
-      // Obter o ícone correspondente à categoria
       Map<String, dynamic>? categoria = (transacao['tipo'] == 'receita')
           ? categoriasReceitas.firstWhere(
               (cat) => cat['nome'] == transacao['categoria'],
@@ -371,7 +371,7 @@ Widget _buildBalanceDetail(String title, String value, Color valueColor) {
             'R\$ ${transacao['valor']}',
             'Data: $formattedDate',
             cor,
-            iconeCategoria, // Passar o ícone da categoria
+            iconeCategoria,
           ),
           const SizedBox(height: 10),
         ],
@@ -393,7 +393,7 @@ Widget _buildBalanceDetail(String title, String value, Color valueColor) {
         children: [
           Row(
             children: [
-              Icon(icone, color: textColor), // Exibir o ícone aqui
+              Icon(icone, color: textColor),
               const SizedBox(width: 10),
               Text(
                 title,
